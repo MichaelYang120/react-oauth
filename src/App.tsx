6fb046f737bcf6cf46ab10ebffb6c095d5628414
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-10-03 18:03:53 -0500

    added form and submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import Input from './components/Inputs'
 import Home from './pages/home'
 
 function App() {
 
   return (
     <>
-		<div>asdf</div>
 		<Home/>
-		<Input name="name" label="name"/>
     </>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,24 @@
+import React from "react";
+import Input from "../components/Inputs";
+
 export default function Home() {
+	const handleSubmit = (event:React.SyntheticEvent) => {
+		event.preventDefault();
+		let debug:boolean = false;
+		if(debug) {
+			console.log(event);
+		}
+	}
+
   return (
 	<>
-	  <div>is this working?</div>
+		<div>Sign In</div>
+		<form onSubmit={handleSubmit}>
+			<Input name="Name" label="Name"/>
+			<Input name="Email" label="Email"/>
+			<Input name="Password" label="Password"/>
+			<button type="submit">Submit</button>
+		</form>
 	</>
   )
 }
