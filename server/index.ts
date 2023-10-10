83f4b7513fd7c7437bd726a9f583c60bb198093d
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-10-10 17:36:10 -0500

    updated server directory

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3003;
+//const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, world!");
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
